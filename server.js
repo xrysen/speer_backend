4f392ba4666853a50b951fc00ecc4885ebd73588
Sean Oyler <xrysen@gmail.com>
2021-05-08 11:48:40 -0700

    Minor code cleanup

--- server.js
+++ server.js
@@ -18,34 +18,42 @@ app.use(
 );
 
 app.post("/register", (req, res) => {
-  db.getUserByName(req.body.userName).then((result) => {
-    if (result.rows.length) {
-      res.status(400).send("A user with that name already exists. Please try another");
-    } else {
-      db.registerNewUser(
-        req.body.name,
-        req.body.userName,
-        req.body.email,
-        bcrypt.hashSync(req.body.password, 10)
-      );
-    }
-  })
-  .then(() => res.status(200).send("User added successfully"))
-  .catch(err => res.status(400).send(err));
+  db.getUserByName(req.body.userName)
+    .then((result) => {
+      if (result.rows.length) {
+        res
+          .status(400)
+          .send("A user with that name already exists. Please try another");
+      } else {
+        db.registerNewUser(
+          req.body.name,
+          req.body.userName,
+          req.body.email,
+          bcrypt.hashSync(req.body.password, 10)
+        );
+      }
+    })
+    .then(() => res.status(200).send("User added successfully"))
+    .catch((err) => res.status(400).send(err));
 });
 
 app.post("/login", (req, res) => {
-  db.getUserByName(req.body.userName).then((result) => {
-    // Check if user name exists and password is correct
-    if (!result.rows.length || !bcrypt.compareSync(req.body.password, result.rows[0].password)) {
-      res.status(400).send("Either the user name or password are incorrect");
-    } else {
-      // Login user and start cookie session
-      req.session.userId = result.rows[0].id;
-      res.status(200).send("Succesfully logged in");
-    }
-  })
-})
+  db.getUserByName(req.body.userName)
+    .then((result) => {
+      // Check if user name exists and password is correct
+      if (
+        !result.rows.length ||
+        !bcrypt.compareSync(req.body.password, result.rows[0].password)
+      ) {
+        res.status(400).send("Either the user name or password are incorrect");
+      } else {
+        // Login user and start cookie session
+        req.session.userId = result.rows[0].id;
+        res.status(200).send("Succesfully logged in");
+      }
+    })
+    .catch((err) => console.log(err));
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
