d628f79b445d751d9cf84246a01dc77d0d922b67
Sean Oyler <xrysen@gmail.com>
2021-05-08 22:05:11 -0700

    Adds tests for put and delete tweets

--- test/tweetTests.js
+++ test/tweetTests.js
@@ -3,7 +3,6 @@ const chaiHTTP = require("chai-http");
 const server = require("../server");
 const should = chai.should();
 const db = require("../database/db");
-const bcrypt = require("bcrypt");
 const { expect } = require("chai");
 
 process.env.NODE_ENV = "test";
@@ -12,18 +11,20 @@ chai.use(chaiHTTP);
 
 describe("Creating tweets", () => {
   before(() => {
-    const user = {
-      userName: "sassysally",
-      password: "ComeHackMeBro",
-    };
     db.pool.query(
       `DROP TABLE IF EXISTS tweets CASCADE;
        CREATE TABLE tweets (
-         id SERIAL PRIMARY KEY NOT NULL,
-         body TEXT NOT NULL,
-         user_id INTEGER REFERENCES users(ud) ON DELETE CASCADE,
-         date TIMESTAMP NOT NULL
-       );
+        id SERIAL PRIMARY KEY NOT NULL,
+        body TEXT NOT NULL,
+        user_id INTEGER REFERENCES users(id) NOT NULL,
+        date TIMESTAMP NOT NULL
+      );
+
+      INSERT INTO tweets (body, user_id, date)
+      VALUES ('Just a small town girl', 1, NOW());
+
+      INSERT INTO tweets (body, user_id, date) 
+      VALUES ('Living in a lonely world', 1, NOW());
       `
     );
   });
@@ -42,10 +43,12 @@ describe("Creating tweets", () => {
         res.should.have.status(400);
       });
   });
-  
+
   it("Shouldn't allow a user to delete a tweet if not logged in", () => {
     chai
-      .request(server).get("/tweets").end((err, res) => res.should.have.status(400));
+      .request(server)
+      .get("/tweets")
+      .end((err, res) => res.should.have.status(400));
   });
 
   it("Should allow a logged in user to create a tweet", () => {
@@ -57,8 +60,42 @@ describe("Creating tweets", () => {
         return agent
           .post("/tweets")
           .send({ msgBody: "Testing Tweets" })
-          .end((err, res) => { res.should.have.status(200); agent.close(); });
+          .end((err, res) => {
+            res.should.have.status(200);
+            agent.close();
+          });
       });
   });
 
+  it("Should allow a logged in user to edit their tweet", () => {
+    const agent = chai.request.agent(server);
+    agent
+      .post("/login")
+      .send({ userName: "sassysally", password: "ComeHackMeBro" })
+      .end((err, res) => {
+        return agent
+          .put("/tweets")
+          .send({ id: 1, msg: "Updated tweet" })
+          .end((err, res) => {
+            res.should.have.status(200);
+            agent.close();
+          });
+      });
+  });
+
+  it("Should allow a logged in user to delete a tweet", () => {
+    const agent = chai.request.agent(server);
+    agent
+      .post("/login")
+      .send({ userName: "sassysally", password: "ComeHackMeBro" })
+      .end((err, res) => {
+        return agent
+          .delete("/tweets")
+          .send({ tweetId: 2 })
+          .end((err, res) => {
+            res.should.have.status(200);
+            agent.close();
+          });
+      });
+  });
 });
